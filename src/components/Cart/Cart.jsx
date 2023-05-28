2e438959d2139fc87363e20a44dd26ae3dd0b585
Sadik <121496253+Sadik-Al-Sami@users.noreply.github.com>
2023-05-28 17:48:15 +0600

    Product calculation added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,9 +4,12 @@ const Cart = (props) => {
     const { cart } = props;
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0
     for (const product of cart){
-        total = total + product.price;
+        // product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = total * 0.07
     const grandTotal = total + totalShipping + tax;
@@ -14,7 +17,7 @@ const Cart = (props) => {
         <div className='cart'>
             <h2>Order Summery</h2>
             <div>
-                <p>Selected Items {cart.length}</p>
+                <p>Selected Items {quantity}</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping Charge: ${totalShipping}</p>
                 <p>Tax: ${tax.toFixed(2)}</p>
